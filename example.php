14c26663f5924583a64fd5565b713528f77a39ef
Your Name <you@example.com>
2021-03-26 18:58:09 -0300

    ..

---
+++ example.php
@@ -0,0 +1,16 @@
+Olá mundo! ${"\t"} ${name}
+${texto}
+${"\n"}
+${2+2}
+${2*20}
+${PHP: 'ola mundo'}
+${PHP i = 0; while(i < 10){i++; echo i;}}
+
+<script>
+    let name = 'Andressa';
+    let texto = 'texto legal';
+</script>
+
+
+<!-- include script -->
+<script src="jsCallVar.js"></script>
