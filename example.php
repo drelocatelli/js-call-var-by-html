48e51ed4d62b6ed642870ff40c67183c51443ade
Your Name <you@example.com>
2021-03-26 19:26:29 -0300

    ..

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+    $variavelPHP = 'variavel php';
+?>
+
+Olá mundo! ${"\t"} ${name}
+${texto}
+${"\n"}
+${2+2}
+${2*20}
+${PHP: 'ola mundo'}
+${PHP: $variavelPHP}
+
+<script>
+    let name = 'Andressa';
+    let texto = 'texto legal';
+</script>
+
+
+<!-- include script -->
+<script src="jsCallVar.js"></script>
