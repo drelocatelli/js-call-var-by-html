3463d6e90bd0ed8c0bc78d2ddbbc3ac008bf6d91
Your Name <you@example.com>
2021-03-26 18:24:43 -0300

    ..

--- jsCallVar.js
+++ jsCallVar.js
@@ -3,6 +3,15 @@ const variables = /\${(.*?)}/gm
 let finds = document.body.innerHTML.match(variables)
 
 finds.forEach(function(i){
+    
     let variableName = i.replace('$','').replace('{','').replace('}','')
-    document.body.innerHTML = document.body.innerHTML.replace(i, eval(variableName))
+    variableName = eval(variableName)    
+    
+    if(variableName == '\n'){
+        variableName = '<br>'
+    }else if(variableName == '\t'){
+        variableName = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+    }
+    
+    document.body.innerHTML = document.body.innerHTML.replace(i, variableName)
 });
