53ddca43dab75e80f8807107073972d88b005eb7
Your Name <you@example.com>
2021-03-26 19:40:01 -0300

    ..

--- jsCallVar.js
+++ jsCallVar.js
@@ -10,8 +10,6 @@ finds.forEach(function(i){
         variableName = variableName.replace('PHP: ', '')
         variableName = `<?php echo ${variableName} ?>`
         
-    }else if(variableName == ){
-
     }else{
         variableName = eval(variableName) 
         // placeholders
