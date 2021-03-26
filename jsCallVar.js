524964b8f0f373e980e68bd5ae78b92ee40593c6
Your Name <you@example.com>
2021-03-26 19:54:30 -0300

    ..

---
+++ jsCallVar.js
@@ -0,0 +1,22 @@
+const variables = /\${(.*?)}/gm
+
+let finds = document.body.innerHTML.match(variables)
+
+finds.forEach(function(i){
+    
+    let variableName = i.replace('${','').replace('}','')
+
+    if((/\$[A-Za-z]*/gm).test(variableName)){
+        variableName = `<?php echo ${variableName}; ?>`
+    }else{
+        variableName = eval(variableName) 
+        // placeholders
+        if(variableName == '\n'){
+            variableName = '<br>'
+        }else if(variableName == '\t'){
+            variableName = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+        }
+    }
+
+    document.body.innerHTML = document.body.innerHTML.replace(i, variableName)
+});
