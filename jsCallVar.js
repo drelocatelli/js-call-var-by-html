06e7a359334d0df524ee48805283803f880035fd
Your Name <you@example.com>
2021-03-26 19:19:48 -0300

    ..

--- jsCallVar.js
+++ jsCallVar.js
@@ -7,7 +7,7 @@ finds.forEach(function(i){
     let variableName = i.replace('$','').replace('{','').replace('}','')
 
     if(variableName == /PHP: [a-zA-Z0-9 ]*/gm){
-        variableName = '\$'+variableName
+        variableName = '$'+variableName
         variableName = `<?php echo ${variableName}; ?>`
         
     }else if(variableName == /PHP: '[a-zA-Z0-9 ]*'/gm){
